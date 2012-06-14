d8e5d1277456d6a48495ecd547f355e4704672cc
meri <meri@meri.org>
2012-06-14 11:35:14 +0200

    First commit.

---
+++ src/test/java/org/porting/less4j/InitialTest.java
@@ -0,0 +1,14 @@
+package org.porting.less4j;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class InitialTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
