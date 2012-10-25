a56479741258b5858e6d8d88be2b32b599a02d6b
meri <meri@meri.org>
2012-10-25 14:13:28 +0200

    Lazy loaded namespaces are done now.

--- src/test/java/com/github/sommeri/less4j/compiler/NamespacesTest.java
+++ src/test/java/com/github/sommeri/less4j/compiler/NamespacesTest.java
@@ -10,7 +10,7 @@ import com.github.sommeri.less4j.utils.w3ctestsextractor.TestFileUtils;
 public class NamespacesTest extends BasicFeaturesTests {
   
   private static final String standardCases = "src/test/resources/compile-basic-features/namespaces/";
-//  private static final String lessjsIncompatible = "src/test/resources/compile-basic-features/mixins/lessjs-incompatible";
+  private static final String lessjsIncompatible = "src/test/resources/compile-basic-features/namespaces/lessjs-incompatible";
 //  private static final String lessjsTests = "src/test/resources/compile-basic-features/mixins/less.js";
 
   public NamespacesTest(File inputFile, File outputFile, String testName) {
@@ -19,7 +19,8 @@ public class NamespacesTest extends BasicFeaturesTests {
 
   @Parameters()
   public static Collection<Object[]> allTestsParameters() {
-    return (new TestFileUtils()).loadTestFiles(standardCases);
+    //return (new TestFileUtils()).loadTestFile(standardCases+"todo/", "namespaces-scoping-mixins-A.less");
+    return (new TestFileUtils()).loadTestFiles(standardCases, lessjsIncompatible);
   }
 
 }
