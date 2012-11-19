bf5c69c1f7de6b83380faa894b8f318b7ba56ba6
meri <meri@meri.org>
2012-11-19 12:55:29 +0100

    ammend

--- src/main/java/com/github/sommeri/less4j/commandline/Arguments.java
+++ src/main/java/com/github/sommeri/less4j/commandline/Arguments.java
@@ -25,6 +25,7 @@ public class Arguments {
   @Parameter(names={"-o", "--outputDir"}, description="Specify the output directory. If not present, Less4j will place output files into current directory. This option is available only in multiple files mode.")
   private String outputDirectory;
 
+  //FIXME: add this to wiki
   @Parameter(names={"-pi", "--printIncorrect"}, description="Print result of syntactically incorrect input file. If not present, syntactically incorrect input files produce no result. If present, less4j prints whatever came out of the compilation as a compilation result - most likely an invalid CSS. ")
   private boolean printIncorrect;
 
