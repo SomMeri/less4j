083f3845b73ddc86e3ad9d5828045dc284e8ecf3
karlvr <karl@xk72.com>
2013-01-22 17:23:02 +1300

    Tests referenced incorrect resources (swapped)

--- src/test/java/com/github/sommeri/less4j/compiler/CssOptimizationsTest.java
+++ src/test/java/com/github/sommeri/less4j/compiler/CssOptimizationsTest.java
@@ -9,7 +9,7 @@ import com.github.sommeri.less4j.utils.TestFileUtils;
 
 public class CssOptimizationsTest extends BasicFeaturesTest {
 
-  private static final String standardCases = "src/test/resources/compile-basic-features/functions/";
+  private static final String standardCases = "src/test/resources/compile-basic-features/css-optimizations/";
 
   public CssOptimizationsTest(File inputFile, File outputFile, String testName) {
     super(inputFile, outputFile, testName);
