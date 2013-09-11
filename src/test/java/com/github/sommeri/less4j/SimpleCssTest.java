e43ec1d874bf925fc54c724c09555fde2ff2d14c
meri <meri@meri.org>
2013-09-11 22:58:11 +0200

    Missing import

--- src/test/java/com/github/sommeri/less4j/SimpleCssTest.java
+++ src/test/java/com/github/sommeri/less4j/SimpleCssTest.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 
 import org.apache.commons.io.FileUtils;
+import org.junit.Ignore;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
