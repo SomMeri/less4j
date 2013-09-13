ebd5adc7ee44fc53c31ba16f4316aa1e96951edc
meri <meri@meri.org>
2013-09-13 14:24:40 +0200

    ... refactoring ...

--- src/main/java/com/github/sommeri/less4j/core/compiler/scopes/IScope.java
+++ src/main/java/com/github/sommeri/less4j/core/compiler/scopes/IScope.java
@@ -1,20 +1,12 @@
 package com.github.sommeri.less4j.core.compiler.scopes;
 
-import java.util.List;
-
 import com.github.sommeri.less4j.core.ast.ASTCssNode;
 import com.github.sommeri.less4j.core.ast.Expression;
 import com.github.sommeri.less4j.core.ast.Variable;
 import com.github.sommeri.less4j.core.compiler.scopes.refactoring.ILocalScope;
 import com.github.sommeri.less4j.core.compiler.scopes.refactoring.ISurroundingScopes;
 
-public interface IScope extends ILocalScope {
-
-  public IScope getParent();
-
-  public List<IScope> getChilds();
-
-  public boolean hasParent();
+public interface IScope extends ILocalScope, ISurroundingScopes {
 
   public String toString();
 
@@ -26,8 +18,6 @@ public interface IScope extends ILocalScope {
   public IScope skipBodyOwner();
 
   public String toLongString();
-
-  public void setParent(IScope parent);
 
   public void removedFromAst();
 
@@ -41,17 +31,17 @@ public interface IScope extends ILocalScope {
 
   public IScope childByOwners(ASTCssNode headNode, ASTCssNode... restNodes);
 
-  public int getTreeSize();
-
   //FIXME (!!!) these used to be private
   public StringBuilder toLongString(int level);
-
-  public void addChild(IScope child);
 
   public ASTCssNode getOwner();
 
   //FIXME (!!!) these did not exists at all 
   public ILocalScope getLocalScope();
+
+  public Expression getValue(Variable variable);
+
+  public Expression getValue(String name);
 
   public Expression getLocalValue(Variable variable);
 
--- src/main/java/com/github/sommeri/less4j/core/compiler/scopes/refactoring/BasicScope.java
+++ src/main/java/com/github/sommeri/less4j/core/compiler/scopes/refactoring/BasicScope.java
@@ -4,64 +4,31 @@ import java.util.Iterator;
 import java.util.List;
 
 import com.github.sommeri.less4j.core.ast.ASTCssNode;
-import com.github.sommeri.less4j.core.ast.AbstractVariableDeclaration;
 import com.github.sommeri.less4j.core.ast.Expression;
-import com.github.sommeri.less4j.core.ast.ReusableStructure;
-import com.github.sommeri.less4j.core.ast.ReusableStructureName;
 import com.github.sommeri.less4j.core.ast.Variable;
-import com.github.sommeri.less4j.core.compiler.expressions.ExpressionFilter;
-import com.github.sommeri.less4j.core.compiler.scopes.FullMixinDefinition;
 import com.github.sommeri.less4j.core.compiler.scopes.IScope;
-import com.github.sommeri.less4j.core.compiler.scopes.LocalScopeData;
-import com.github.sommeri.less4j.core.compiler.scopes.MixinsDefinitionsStorage;
-import com.github.sommeri.less4j.core.compiler.scopes.VariablesDeclarationsStorage;
 
-public class BasicScope implements IScope {
+public class BasicScope extends ComposedDumbScope implements IScope {
 
-  private ILocalScope localScope;
-  private ISurroundingScopes surroundingScopes;
-
-  public BasicScope(ILocalScope localScope, ISurroundingScopes tree) {
-    this.localScope = localScope;
-    this.surroundingScopes = tree;
+  public BasicScope(ILocalScope localScope, ISurroundingScopes surroundingScopes) {
+    super(localScope, surroundingScopes);
   }
 
-  public ISurroundingScopes getSurroundingScopes() {
-    return surroundingScopes;
-  }
-
-  public void setSurroundingScopes(ISurroundingScopes surroundingScopes) {
-    this.surroundingScopes = surroundingScopes;
-  }
-
-  public ILocalScope getLocalScope() {
-    return localScope;
-  }
-
-  /* ******************************************************************** */
-  public IScope getParent() {
-    return surroundingScopes.getParent();
-  }
-
-  public void addChild(IScope child) {
-    surroundingScopes.addChild(child);
-  }
-
-  public List<IScope> getChilds() {
-    return surroundingScopes.getChilds();
-  }
-
-  public int getTreeSize() {
-    return surroundingScopes.getTreeSize();
-  }
-
-  /* ******************************************************************** */
   public IScope firstChild() {
     return getChilds().get(0);
   }
 
-  public boolean hasParent() {
-    return getParent() != null;
+  public Expression getValue(Variable variable) {
+    return getValue(variable.getName());
+  }
+  
+  public Expression getValue(String name) {
+    Expression value = getLocalValue(name);
+    
+    if (value == null && hasParent())
+      value = getParent().getValue(name);
+    
+    return value;
   }
 
   public IScope childByOwners(ASTCssNode headNode, ASTCssNode... restNodes) {
@@ -110,153 +77,6 @@ public class BasicScope implements IScope {
 
     return this;
   }
-
-  /* ******************************************************************** */
-
-  public Expression getValue(Variable variable) {
-    return getValue(variable.getName());
-  }
-  
-  public Expression getValue(String name) {
-    Expression value = getLocalValue(name);
-    
-    if (value == null && hasParent())
-      value = getParent().getValue(name);
-    
-    return value;
-  }
-
-
-  /* ******************************************************************** */
-  public void setLocalScope(ILocalScope localScope) {
-    this.localScope = localScope;
-  }
-
-  public void addNames(List<String> names) {
-    localScope.addNames(names);
-  }
-
-  public List<String> getNames() {
-    return localScope.getNames();
-  }
-
-  public ASTCssNode getOwner() {
-    return localScope.getOwner();
-  }
-
-  public String getType() {
-    return localScope.getType();
-  }
-
-  public boolean hasTheSameLocalData(ILocalScope otherScope) {
-    return localScope.hasTheSameLocalData(otherScope);
-  }
-
-  public void registerVariable(AbstractVariableDeclaration declaration) {
-    localScope.registerVariable(declaration);
-  }
-
-  public void registerVariable(AbstractVariableDeclaration node, Expression replacementValue) {
-    localScope.registerVariable(node, replacementValue);
-  }
-
-  public void registerVariableIfNotPresent(String name, Expression replacementValue) {
-    localScope.registerVariableIfNotPresent(name, replacementValue);
-  }
-
-  public void registerVariable(String name, Expression replacementValue) {
-    localScope.registerVariable(name, replacementValue);
-  }
-
-  public void addFilteredVariables(ExpressionFilter filter, IScope source) {
-    localScope.addFilteredVariables(filter, source);
-  }
-
-  public void registerMixin(ReusableStructure mixin, IScope mixinsBodyScope) {
-    localScope.registerMixin(mixin, mixinsBodyScope);
-  }
-
-  public void createPlaceholder() {
-    localScope.createPlaceholder();
-  }
-
-  public void addToPlaceholder(IScope otherScope) {
-    localScope.addToPlaceholder(otherScope);
-  }
-
-  public void closePlaceholder() {
-    localScope.closePlaceholder();
-  }
-
-  public void addAllMixins(List<FullMixinDefinition> mixins) {
-    localScope.addAllMixins(mixins);
-  }
-
-  public void add(IScope otherSope) {
-    localScope.add(otherSope);
-  }
-
-  public void addVariables(IScope otherSope) {
-    localScope.addVariables(otherSope);
-  }
-
-  public List<FullMixinDefinition> getAllMixins() {
-    return localScope.getAllMixins();
-  }
-
-  public List<FullMixinDefinition> getMixinsByName(List<String> nameChain, ReusableStructureName name) {
-    return localScope.getMixinsByName(nameChain, name);
-  }
-
-  public List<FullMixinDefinition> getMixinsByName(ReusableStructureName name) {
-    return localScope.getMixinsByName(name);
-  }
-
-  public List<FullMixinDefinition> getMixinsByName(String name) {
-    return localScope.getMixinsByName(name);
-  }
-
-  public MixinsDefinitionsStorage getLocalMixins() {
-    return localScope.getLocalMixins();
-  }
-
-  public VariablesDeclarationsStorage getLocalVariables() {
-    return localScope.getLocalVariables();
-  }
-
-  public LocalScopeData getLocalData() {
-    return localScope.getLocalData();
-  }
-
-  public void createLocalDataSnapshot() {
-    localScope.createLocalDataSnapshot();
-  }
-
-  public void discardLastLocalDataSnapshot() {
-    localScope.discardLastLocalDataSnapshot();
-  }
-
-  public boolean isBodyOwnerScope() {
-    return localScope.isBodyOwnerScope();
-  }
-
-  public Expression getLocalValue(Variable variable) {
-    return localScope.getValue(variable);
-  }
-
-  public Expression getLocalValue(String name) {
-    return localScope.getValue(name);
-  }
-
-  public void removedFromAst() {
-    localScope.removedFromAst();
-  }
-
-  public boolean isPresentInAst() {
-    return localScope.isPresentInAst();
-  }
-
-  /* ******************************************************************** */
 
   public void setParent(IScope parent) {
     if (getSurroundingScopes().hasParent()) {
