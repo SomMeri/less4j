b1b9d20ba0553dc22fb9c2ce4a87162a70bdb1fa
meri <meri@meri.org>
2013-10-05 12:23:24 +0200

    Added some more test cases for basic extend version #151

--- src/test/java/com/github/sommeri/less4j/compiler/ExtendsTest.java
+++ src/test/java/com/github/sommeri/less4j/compiler/ExtendsTest.java
@@ -6,8 +6,9 @@ import java.util.Collection;
 import org.junit.Ignore;
 import org.junit.runners.Parameterized.Parameters;
 
+//FIXME (!!!) does and should it extend rulesets in media?
 //FIXME (!!!) add tests for values lists for attributes - e.g. error handling
-//@Ignore
+@Ignore
 public class ExtendsTest extends BasicFeaturesTest {
 
   private static final String standardCases = "src/test/resources/compile-basic-features/extends/";
