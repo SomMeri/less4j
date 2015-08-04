683253e7cff98ee54f399578f1a7bd93160e28a7
jurcovicovam <meri@meri.org>
2015-08-04 13:50:37 +0200

    Cleaned up #292

--- src/main/java/com/github/sommeri/less4j/core/TimeoutedLessCompiler.java
+++ src/main/java/com/github/sommeri/less4j/core/TimeoutedLessCompiler.java
@@ -9,24 +9,30 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
 
 import com.github.sommeri.less4j.Less4jException;
 import com.github.sommeri.less4j.LessCompiler;
 import com.github.sommeri.less4j.LessSource;
 import com.github.sommeri.less4j.core.ast.ASTCssNode;
 import com.github.sommeri.less4j.core.problems.BugHappened;
-import com.github.sommeri.less4j.core.problems.ProblemsHandler;
 
 public class TimeoutedLessCompiler implements LessCompiler {
 
   private final long timeout;
   private final TimeUnit unit;
+  private final long afterInterruptTimeout;
+  private final TimeUnit afterInterruptUnit;
 
   public TimeoutedLessCompiler(long timeout, TimeUnit unit) {
+    this(timeout, unit, 80, TimeUnit.MILLISECONDS);
+  }
+
+  public TimeoutedLessCompiler(long timeout, TimeUnit unit, long afterInterruptTimeout, TimeUnit afterInterruptunit) {
     super();
     this.timeout = timeout;
     this.unit = unit;
+    this.afterInterruptTimeout = afterInterruptTimeout;
+    this.afterInterruptUnit = afterInterruptunit;
   }
 
   @Override
@@ -87,22 +93,20 @@ public class TimeoutedLessCompiler implements LessCompiler {
     try {
       boolean terminatedNormally = executor.awaitTermination(timeout, unit);
       if (terminatedNormally) {
-        try {
-          CompilationResult result = future.get();
-          return result;
-        } catch (ExecutionException e) {
-          throw (Less4jException) e.getCause().getCause();
-        }
+        CompilationResult result = future.get();
+        return result;
       } else {
         executor.shutdownNow();
-        executor.awaitTermination(80, unit);
+        executor.awaitTermination(afterInterruptTimeout, afterInterruptUnit);
         return future.get();
       }
+    } catch (CancellationException e) {
+      throw new BugHappened("Unexpected future cancellation.", (ASTCssNode) null);
     } catch (InterruptedException e) {
       throw new BugHappened("Unexpected thread interrupt.", (ASTCssNode) null);
     } catch (ExecutionException e) {
       Throwable cause = e.getCause();
-      
+
       if (cause instanceof Less4jRuntimeException) {
         Less4jRuntimeException wrapper = (Less4jRuntimeException) cause;
         throw (Less4jException) wrapper.getCause();
@@ -122,63 +126,12 @@ public class TimeoutedLessCompiler implements LessCompiler {
     }
   }
 
-  //  @Override
-  //  public CompilationResult compile(LessSource source, Configuration options) throws Less4jException {
-  //    final LessSource iSource = source;
-  //    final Configuration iOption = options;
-  //
-  //    Callable<CompilationResult> task = new Callable<CompilationResult>() {
-  //      public CompilationResult call() {
-  //
-  //        ThreadUnsafeLessCompiler compiler = new ThreadUnsafeLessCompiler();
-  //        try {
-  //          return compiler.compile(iSource, iOption);
-  //        } catch (Less4jException ex) {
-  //          throw new RuntimeException(ex);
-  //        }
-  //      }
-  //    };
-  //
-  //    ExecutorService executor = Executors.newSingleThreadExecutor();
-  //    Future<CompilationResult> future = executor.submit(task);
-  //    try {
-  //      CompilationResult result = future.get(timeout, unit);
-  //      return result;
-  //    } catch (InterruptedException e) {
-  //      throw new BugHappened("Unexpected thread interrupt.", (ASTCssNode) null);
-  //    } catch (ExecutionException e) {
-  //      throw (Less4jException) e.getCause().getCause();
-  //    } catch (TimeoutException e) {
-  //      if (future.cancel(true)) {
-  ////        ProblemsHandler problemsHandler = new ProblemsHandler();
-  ////        problemsHandler.unableToFinish(lessStyleSheet, ex);
-  ////        return createEmptyCompilationResult();
-  //        return getResult(future);
-  //      }
-  //      return getResult(future);
-  //    }
-  //
-  //  }
-//
-//  private CompilationResult getResult(Future<CompilationResult> future) throws Less4jException {
-//    try {
-//      return future.get();
-//    } catch (CancellationException ex) {
-//      System.out.println(ex);
-//      return null;
-//    } catch (InterruptedException e1) {
-//      throw new BugHappened("Unexpected thread interrupt.", (ASTCssNode) null);
-//    } catch (ExecutionException e1) {
-//      throw (Less4jException) e1.getCause().getCause();
-//    }
-//  }
-
   @SuppressWarnings("serial")
   private class Less4jRuntimeException extends RuntimeException {
 
     public Less4jRuntimeException(Less4jException ex) {
       super(ex);
     }
-    
+
   }
 }
